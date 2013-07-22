09058ed6ce7df8358b26d184474807075008edff
JulesM <jules.marcilhacy@gmail.com>
2013-07-22 19:23:58 +0200

    simplification

--- web/js/creation-process.js
+++ web/js/creation-process.js
@@ -10,11 +10,7 @@ $(document).ready(function() {
 	// show only facebook friends matching input
 	$("#friend-input").keyup(function() {
 		input_content = $(this).val();
-		if (input_content === ''){
-			$('#friend-table .name').closest("tr").show();
-		}else{
-			$('#friend-table .name').closest("tr").show().find(".name").not(':contains(' + input_content + ')').closest("tr").hide();
-		}
+		$('#friend-table .name').closest("tr").show().find(".name").not(':contains(' + input_content + ')').closest("tr").hide();
 	});
 	// Scroll in friend table
 $("#friend-table").slimScroll({
