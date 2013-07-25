ed07712d1544b7c31bc41d11c76a86e7f8c54496
JulesM <jules.marcilhacy@gmail.com>
2013-07-25 12:28:53 +0200

    list cleaning

--- src/Tk/ListBundle/Controller/ListController.php
+++ src/Tk/ListBundle/Controller/ListController.php
@@ -15,16 +15,4 @@ class ListController extends Controller
             return $this->render('TkListBundle::index.html.twig');
         }
     }
-
-    private function getAllTodosAction()
-    {
-    	$todos_service = $this->container->get('tk_list.todos');
-    	return $todos_service->getAllTodos($this->getUser()->getCurrentMember()->getTGroup());
-    }
-
-    private function getAllShoppingItemsAction()
-    {
-    	$shopping_service = $this->container->get('tk_list.shopping');
-    	return $shopping_service->getAllShoppingItems($this->getUser()->getCurrentMember()->getTGroup());
-    }
 }
